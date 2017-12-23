90c8c09f84be8b686aaffa368ef19d9abadd42da
Ricardo Barbosa <barbosa.cisco@gmail.com>
2017-12-23 16:18:57 +0000

    Removed the content of the scripts as requested.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
 echo DEPLOY
-
-sudo docker pull jenkins
-
-sudo docker run -p 8080:8080 --name=jenkins-master jenkins
-
-
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
 echo TEST
-
-SERVICE="docker"
-RESULT=`ps -a | sed -n /${SERVICE}/p`
-
-if [ "${RESULT:-null}" = null ]; then
-    echo "Docker is not running"
-else
-    echo "Docker is running"
-fi
-
-
