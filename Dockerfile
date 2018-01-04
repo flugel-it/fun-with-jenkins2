00a871624c1ccc812609d91811f31ae3a2d2d6f6
root <orest.hopiak@sol-ra.com>
2018-01-04 19:37:53 +0200

    Fixed ansible playbook, added some portability

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
-
 FROM jenkinsci/jenkins:latest
+
+USER root
+
+COPY id_rsa /opt/id_rsa
+RUN chown jenkins:jenkins /opt/id_rsa
+
+USER jenkins
+
 RUN /usr/local/bin/install-plugins.sh workflow-aggregator job-dsl git build-flow-plugin
+
 ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
+
 COPY admin.groovy /usr/share/jenkins/ref/init.groovy.d/
 COPY init.groovy /usr/share/jenkins/ref/init.groovy.d/
-COPY id_rsa /opt/id_rsa
 
---
+++ init.groovy
@@ -0,0 +1,53 @@
+#!groovy
+import jenkins.*;
+import jenkins.model.*;
+import hudson.model.*;
+import hudson.triggers.SCMTrigger;
+import hudson.plugins.git.GitSCM;
+import hudson.plugins.git.BranchSpec;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
+import javaposse.jobdsl.plugin.*;
+import hudson.security.csrf.DefaultCrumbIssuer;
+
+jenkins = Jenkins.instance;
+jenkins.save();
+
+GlobalConfiguration.all().get(GlobalJobDslSecurityConfiguration.class).useScriptSecurity=false
+GlobalConfiguration.all().get(GlobalJobDslSecurityConfiguration.class).save()
+
+credentialsStore = Jenkins.instance.getExtensionList(com.cloudbees.plugins.credentials.SystemCredentialsProvider.class)[0];
+privateKey = new BasicSSHUserPrivateKey(CredentialsScope.GLOBAL,"jenkins-key","jenkins",new BasicSSHUserPrivateKey.FileOnMasterPrivateKeySource('/opt/id_rsa'),"","jenkins ssh key")
+credentialsStore = Jenkins.instance.getExtensionList(com.cloudbees.plugins.credentials.SystemCredentialsProvider.class)[0];
+credentialsStore.store.addCredentials(Domain.global(), privateKey);
+
+dsl_name = "pipelines-dsl-bootstrap"
+
+dsl = new hudson.model.FreeStyleProject(jenkins, dsl_name);
+gitTrigger1 = new SCMTrigger("*/5 * * * *");
+dsl.addTrigger(gitTrigger1);
+dsl.scm = new GitSCM("git@github.com:flugel-it/fun-with-jenkins2.git");
+dsl.scm.branches = [new BranchSpec("*/master")];
+dsl.scm.userRemoteConfigs[0].credentialsId = 'jenkins-key';
+
+String pipelineScripts = "${dsl_name}.groovy".replaceAll("-", "_");
+
+def jobDslBuildStep = new ExecuteDslScripts(
+                            targets: pipelineScripts,
+                            usingScriptText: false,
+                            ignoreExisting: false,
+                            removedJobAction: RemovedJobAction.DELETE,
+                            removedViewAction: RemovedViewAction.IGNORE
+                            );
+
+dsl.getBuildersList().add(jobDslBuildStep);
+
+
+jenkins.add(dsl, dsl_name);
+
+def job1 = jenkins.getItem(dsl.name)
+job1.scheduleBuild()
+
